139a3c95273eb2d6ab721f1bb61a8d475afc9b4e
J.D. Healy <jdhealy@gmail.com>
2018-10-26 15:43:58 -0400

    Structure project and create Xcode configuration.
    - Create `xcconfig`s.
    - Add Jaeger module with directories for Jaeger and OpenTracing source.
    - Add `.gitignore`.
    - `.gitkeep` files are in place to ensure git creates the parent directories upon checkout. Remove each upon placing the first source file in each directory.

---
+++ Sources/Jaeger/Jaeger/Jaeger.h
@@ -0,0 +1,10 @@
+//
+//  Jaeger.h
+//  Jaeger
+//
+
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double JaegerVersionNumber;
+FOUNDATION_EXPORT const unsigned char JaegerVersionString[];
+
---
+++ Sources/Jaeger/Jaeger/Jaeger.swift
@@ -0,0 +1,12 @@
+//
+//  Jaeger.swift
+//  Jaeger
+//
+//  Created by Aaron Sky on 10/23/18.
+//
+
+import Foundation
+
+struct Jaeger {
+    static let text = "Hello World"
+}
