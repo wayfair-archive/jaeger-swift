98d5eafacd69fdb67a14512a07361d05278675f1
Simon-Pierre Roy <sroy1@wayfair.com>
2018-10-29 23:39:33 -0400

    Fixed the test target after deleting a test file.

--- Tests/JaegerTests/JaegerTests.swift
+++ Tests/JaegerTests/JaegerTests.swift
@@ -17,10 +17,4 @@ class JaegerTests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual(Jaeger.text, "Hello World")
-    }
 }
